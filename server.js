a9f18857249706991e9dc07ebf6e2dd2c837f811
mbaibernard3-cmd <mbaibernard3@gmail.com>
2026-06-23 10:27:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const CONSUMER_KEY = "XQ3fQsEBCAOLaMpiUie0tGLZg2aUsCccYXH23vFTbqcpZ8kf";
 const CONSUMER_SECRET = "9gMxGYVFHqsrt6cC1ph5xRgUeLcpmFrdI1J35NStp3DRbYmUctHpVhxhSLKkpA7A";
 const SHORTCODE = "174379"; // Keep as 174379 for sandbox testing
 const PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"; 
-const CALLBACK_URL = "https://your-render-url.onrender.com/api/mpesa-callback"; // You will update this in Step 3
+const CALLBACK_URL = "https://mpesa-backend-l65u.onrender.com/api/mpesa-callback";
